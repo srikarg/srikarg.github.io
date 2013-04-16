3f021fe2c3eca835865ceec583d754d4dd57d4ee
Srikar G <srikarvgudipati@gmail.com>
2013-04-16 17:02:54 -0400

    Added back to top button.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-$(function () {
+$(function() {
     //Contact Info Hover Effect
     var social = $('.social p');
-
-    social.hover(function () {
+    social.hover(function() {
         $(this).children('a').stop().fadeToggle(300);
+    });
+
+    //Smooth scrolling links
+    $('a[href^="#"]').on('click', function() {
+        var target = $(this.hash);
+        if (target.length == 0) target = $('html');
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+        $('body').focus();
+    });
+
+    //Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#top').stop().animate({opacity: 1}, 400).css('pointer-events','auto');
+        } else {
+            $('#top').stop().animate({opacity: 0}, 400).css('pointer-events','none');
+        }
     });
 });
