16e963195c8821f597fb0264f93516b78a5a2347
Srikar G <srikarvgudipati@gmail.com>
2013-04-18 23:16:10 -0400

    Removed smooth scrolling for all hash links and made the back to top a span.

--- js/main.js
+++ js/main.js
@@ -5,18 +5,12 @@ $(function() {
         $(this).children('a').stop().fadeToggle(300);
     });
 
-    //Smooth scrolling links
-    $('.table-of-contents a[href^="#"], #top').on('click', function(e) {
-        e.preventDefault();
-        var target = $(this.hash);
-        if (target.length == 0) target = $('html');
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 500);
-        $('body').focus();
+    //Back to top scroll
+    $('#top').on('click', function(e) {
+        $('html, body').animate({scrollTop:0}, 'slow');
     });
 
-    //Back to top button
+    //Back to top button visibility
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#top').stop().animate({opacity: 1}, 400).css('pointer-events','auto');
