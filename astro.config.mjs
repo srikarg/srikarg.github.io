2488a612eebcc92cd494ac717bb0f740276747db
Srikar Gudipati <srikarvgudipati@gmail.com>
2023-09-12 19:02:44 -0500

    Initial draft of blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import siteConfig from './siteConfig.json'
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: siteConfig.siteUrl,
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 })
---
+++ src/utils/remark-reading-time.mjs
@@ -0,0 +1,14 @@
+// Source: https://docs.astro.build/en/recipes/reading-time/
+
+import getReadingTime from 'reading-time'
+import { toString } from 'mdast-util-to-string'
+
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree)
+    const readingTime = getReadingTime(textOnPage)
+    // readingTime.text will give us minutes read as a friendly string,
+    // i.e. "3 min read"
+    data.astro.frontmatter.minutesRead = readingTime.text
+  }
+}
