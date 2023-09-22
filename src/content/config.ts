70bae96fa59fb62b6a261fbf821de2b8460e156a
Srikar Gudipati <srikarvgudipati@gmail.com>
2023-09-21 20:18:35 -0500

    Initial draft of projects page

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,22 @@ const blogCollection = defineCollection({
     .strict(),
 })
 
+const projectsCollection = defineCollection({
+  type: 'data',
+  schema: ({ image }) =>
+    z
+      .object({
+        name: z.string(),
+        description: z.string(),
+        source_url: z.string().url().optional(),
+        demo_url: z.string().url().optional(),
+        image: image(),
+        image_alt: z.string(),
+      })
+      .strict(),
+})
+
 export const collections = {
   blog: blogCollection,
+  projects: projectsCollection,
 }
