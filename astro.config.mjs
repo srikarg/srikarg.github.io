a928c6addabcbe855d6d4816b6516294a5613803
srikarg <srikarvgudipati@gmail.com>
2024-03-30 13:47:08 -0500

    Add Space Junk Collector game to projects and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
 import tailwind from '@astrojs/tailwind'
-import siteConfig from './siteConfig.json'
+import { siteUrl } from './siteConfig.json'
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind()],
-  site: siteConfig.siteUrl,
+  site: siteUrl,
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
