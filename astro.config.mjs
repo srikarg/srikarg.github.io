5f520eed476944e328649d0631095c058f2c37fb
srikarg <srikarvgudipati@gmail.com>
2025-03-30 15:47:34 -0500

    Upgrade to Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import { siteUrl } from './siteConfig.json'
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()],
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: siteUrl,
   markdown: {
     remarkPlugins: [remarkReadingTime],
